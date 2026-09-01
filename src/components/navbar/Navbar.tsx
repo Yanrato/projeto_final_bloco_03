0b7614ecbd813bd4460c6a1d77486f58fa696c90
Yanrato <yanferreira985@gmail.com>
2026-09-01 11:38:51 -0300

    ✨ Atualiza Navbar: Adiciona ícone de carrinho de compras

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { ShoppingCartIcon } from "@phosphor-icons/react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
@@ -19,6 +20,8 @@ function Navbar() {
         <nav className="hidden md:flex items-center gap-8 font-bold text-md text-black">
           <a href="#" className="hover:underline transition-colors">Produtos</a>
           <Link to="/categorias" className="hover:underline transition-colors">Categorias</Link>
+
+          <ShoppingCartIcon size={28} weight="regular"/>
         </nav>
 
       </header>
