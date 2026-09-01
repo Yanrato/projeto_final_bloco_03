b1ede60ffc158c681146888c7220440369028232
Yanrato <yanferreira985@gmail.com>
2026-09-01 09:46:52 -0300

    ✨ New Feature:   Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
@@ -6,13 +7,15 @@ import Home from "./pages/home/Home";
 function App() {
   return (
     <div className="flex flex-col min-h-screen bg-letra-700">
-      
-      <Navbar />
-      <main className="flex-1 pb-12">
-        <Home />
-      </main>
-      <Footer />
-      
+      <BrowserRouter>
+        <Navbar />
+        <main className="flex-1 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
